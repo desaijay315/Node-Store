385e1d2471d1fe01695a0ed948d12a4050d7ec0a
desaijay315 <desaijay315@gmail.com>
2017-09-24 18:44:47 +0530

    added controller, models for reviews

---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,81 @@
+const axios = require('axios');
+import  {$} from './bling';
+
+const mapOptions = {
+	center : {lat: 43.2 , lng: -79.8 },
+	zoom: 10
+}
+
+function loadPlaces (map, lat=43.2, lng= -79.8) {
+	// body...
+     axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
+             .then(res =>{
+             	const places = res.data;
+             	console.log(places);
+             	if(!places.length){
+                           alert('no places found');
+             	 return false;
+             	}
+
+             	//creat bonds
+             	const bounds = new google.maps.LatLngBounds();
+                        const infoWindow = new google.maps.InfoWindow();
+
+                       const markers = places.map(place =>{
+                     	const [placeLng, placeLat] =  place.location.coordinates;
+                     	const position = {lat: placeLat, lng: placeLng};
+                     	bounds.extend(position);
+                     	const marker = new google.maps.Marker({map, position})
+                     	;
+                     	marker.place = place;
+                     	return marker;
+                     });
+                     //show detailson click
+                     markers.forEach(marker => marker.addListener('click', function(){
+                     	  console.log(this);
+                     	  const html = `
+ 		    <div class="popup">
+ 		       <a href="/store/${this.place.slug}">
+ 		        <img src="/uploads/${this.place.photo || 'store.png' }" alt="${this.place.name}">
+ 		        <p>${this.place.name} - ${this.place.location.address} </p>
+ 		    </div>
+                     	  `;
+                     	  infoWindow.setContent(html);
+                     	  infoWindow.open(map, this);
+                     }));
+
+                      //then zoom the marker to fir that paerfectly
+                      map.setCenter(bounds.getCenter());
+                      map.fitBounds(bounds);
+             })
+}
+
+
+function makeMap(mapDiv){
+  console.log(mapDiv);
+  if(!mapDiv){
+    return;
+  } 
+
+ const map = new google.maps.Map(mapDiv, mapOptions);
+ loadPlaces(map);
+
+ const input = $('[name="geolocate"]');
+ const autocomplete = new google.maps.places.Autocomplete(input);
+console.log(autocomplete);
+ 
+ 
+      autocomplete.addListener('place_changed', () => {
+        const  place = autocomplete.getPlace();
+        console.log(place);
+       const  latInput  = place.geometry.location.lat();
+        console.log("lat "+ latInput);
+        const lngInput  = place.geometry.location.lng();
+        loadPlaces(map,latInput ,lngInput );
+
+     });
+
+
+}
+
+export default makeMap;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router              = express.Router();
 const storeController = require('../controllers/storeController');
 const userController  = require('../controllers/userController');
 const authController  = require('../controllers/authController');
+const reviewController  = require('../controllers/reviewController');
 const {catchErrors}  = require('../handlers/errorHandlers');
 
 // Do work here
@@ -66,8 +67,20 @@ router.post('/account/reset/:token',
 
 //api
 
-router.get('/api/search', catchErrors(storeController.searchStores))
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 
+router.get('/api/stores/near', catchErrors(storeController.mapstore));
+router.get('/map', storeController.mapPage);
+
+router.get('/hearts',authController.isLoggedIn,catchErrors(storeController.getHearts));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
+
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
+
+
+router.get('/top', catchErrors(storeController.getTopStores));
+
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
 
 module.exports = router;
